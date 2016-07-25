92e67e321b0a56a44890ce43cef5f849053e4da9
Your Name <you@example.com>
2016-07-25 11:35:19 +0900

    first commit

---
+++ MyApplication/app/src/main/java/com/example/a/myapplication/MainActivity.java
@@ -0,0 +1,19 @@
+package com.example.a.myapplication;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void onBtnClick(View v){
+        Log.d("dddda","bdddd");
+    }
+}
