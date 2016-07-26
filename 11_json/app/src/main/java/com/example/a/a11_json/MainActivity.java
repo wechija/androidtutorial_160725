5cabfde09b7bddac223d70f01be7a812fc79f55b
Your Name <you@example.com>
2016-07-26 16:45:48 +0900

    json

--- 11_json/app/src/main/java/com/example/a/a11_json/MainActivity.java
+++ 11_json/app/src/main/java/com/example/a/a11_json/MainActivity.java
@@ -1,7 +1,16 @@
 package com.example.a.a11_json;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+
+import com.loopj.android.http.AsyncHttpClient;
+import com.loopj.android.http.JsonHttpResponseHandler;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import cz.msebera.android.httpclient.Header;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +18,23 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        AsyncHttpClient client = new AsyncHttpClient();
+        client.get("http://api.androidhive.info/corayntacts/",new JsonHttpResponseHandler(){
+
+            @Override
+            public void onSuccess(int statusCode, Header[] headers, JSONObject response) {
+                super.onSuccess(statusCode, headers, response);
+                try{
+                    JSONArray array = response.getJSONArray("contacts");
+                    Log.d("::",array.toString());
+                }catch(Exception ex){
+                    ex.printStackTrace();
+                }
+            }
+        });
     }
+
+
+
 }
